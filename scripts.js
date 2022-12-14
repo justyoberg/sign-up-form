711d00081be54f63ffb70a3a8f9d4570629f93fb
Justy Oberg <justyoberg1@gmail.com>
2022-12-13 18:41:05 -0800

    Set button to be disabled if passwords don't match

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const password = document.querySelector("#password");
 const confirmation = document.querySelector("#confirmation");
 const mismatchMessage = document.querySelector(".pass-mismatch");
+const submitButton = document.querySelector("button");
 
 password.addEventListener("input", checkPasswordMatch);
 confirmation.addEventListener("input", checkPasswordMatch);
@@ -11,9 +12,11 @@ function checkPasswordMatch() {
     password.classList.add("mismatch");
     confirmation.classList.add("mismatch");
     mismatchMessage.style.visibility = "visible";
+    submitButton.disabled = true;
   } else {
     password.classList.remove("mismatch");
     confirmation.classList.remove("mismatch");
     mismatchMessage.style.visibility = "hidden";
+    submitButton.disabled = false;
   }
 }
